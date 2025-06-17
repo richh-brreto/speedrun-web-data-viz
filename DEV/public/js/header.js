549db1e695c4820660841c42b517a144c341077f
Guilherme-Silva-Oliveira <guilherme.soliveira@sptech.school>
2025-06-17 14:11:31 -0300

    Commit Final

---
+++ DEV/public/js/header.js
@@ -0,0 +1,9 @@
+let navBar = document.querySelector('#header');
+document.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY;
+  if (scrollTop > 0) {
+    navBar.classList.add('rolar');
+  } else {
+    navBar.classList.remove('rolar');
+  }
+});
--- DEV/src/routes/tanque.js
+++ DEV/src/routes/tanque.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-var aquarioController = require("../controllers/aquarioController");
+var tanqueController = require("../controllers/tanqueController");
 
 router.get("/:empresaId", function (req, res) {
-  aquarioController.buscarAquariosPorEmpresa(req, res);
+  tanqueController.buscarTanquesPorEmpresa(req, res);
 });
 
 router.post("/cadastrar", function (req, res) {
-  aquarioController.cadastrar(req, res);
+  tanqueController.cadastrar(req, res);
 })
 
 module.exports = router;
