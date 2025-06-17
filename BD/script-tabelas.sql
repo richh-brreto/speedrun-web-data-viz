75e448460aa12a347f5679a7ee2ed098e2725f4d
TitoMichel <michel.mascarenhas@sptech.school>
2025-06-17 13:16:38 -0300

    Mudança de script tabelas

--- BD/script-tabelas.sql
+++ BD/script-tabelas.sql
@@ -1,21 +1,18 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+CREATE DATABASE techbeer;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
+USE techbeer;
 
 CREATE TABLE empresa (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+    codigo_ativacao VARCHAR(50),
+	cnpj CHAR(14)
+	
 );
+
+
+insert into empresa values
+(1,'Cervejaria Titos','ABC10','1020304050607');
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
@@ -26,35 +23,56 @@ CREATE TABLE usuario (
 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+create table  tanque(
+id int primary key auto_increment,
+fk_empresa int,
+tipoCerveja varchar(100),
+capacidade decimal (10,2) default (20000),
+foreign key (fk_empresa) references empresa(id)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+INSERT INTO tanque (fk_empresa, tipoCerveja) VALUES (1, 'Artesanal');
+INSERT INTO tanque (fk_empresa, tipoCerveja) VALUES (1, 'Artesanal');
+INSERT INTO tanque (fk_empresa, tipoCerveja) VALUES (1, 'Artesanal');
+INSERT INTO tanque (fk_empresa, tipoCerveja) VALUES (1, 'Artesanal');
+INSERT INTO tanque (fk_empresa, tipoCerveja) VALUES (1, 'Artesanal');
+
+
+create table alertas (
+fk_tanque int,
+id int,
+foreign key (fk_tanque) references tanque(id),
+dataHora datetime default current_timestamp);
+
+INSERT INTO alertas (fk_tanque) VALUES (1);
+INSERT INTO alertas (fk_tanque) VALUES (1);  -- Alerta para o tanque com id 1
+INSERT INTO alertas (fk_tanque) VALUES (1);  -- Alerta para o tanque com id 2
+INSERT INTO alertas (fk_tanque) VALUES (1);  -- Alerta para o tanque com id 3
+INSERT INTO alertas (fk_tanque) VALUES (2);  -- Alerta para o tanque com id 1
+INSERT INTO alertas (fk_tanque) VALUES (3);  -- Alerta para o tanque com id 2
+INSERT INTO alertas (fk_tanque) VALUES (4);  -- Alerta para o tanque com id 2
+
+-- Total de alertas gerais
+SELECT COUNT(*) AS total_alertas
+FROM alertas;
+
+-- Total de alertas para o tanque com id = 1
+SELECT COUNT(*) AS alertas_tanque_1
+FROM alertas
+WHERE fk_tanque = 1;
+
+-- Total de alertas por tanque
+SELECT fk_tanque, COUNT(*) AS total_alertas_por_tanque
+FROM alertas
+GROUP BY fk_tanque;
+
+
+
+
 
 /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+
 
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
